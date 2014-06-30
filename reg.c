254e2329156879d162d96fe7db1dd85c2aced4a7
sspark1973 <sspark1973@gmail.com>
2014-06-30 16:41:53 -0700

    Create reg.c
    Set Register

---
+++ reg.c
@@ -0,0 +1,13 @@
+typedef long unsigned int dword;
+volatile dword *pwr_cfg;
+pwr_cfg = (volatile dword *)0x7E00F804;
+
+void set_pwr_cfg(dword value)
+{
+  *pwr_cfg = value;
+}
+
+void main()
+{
+  return;
+}
