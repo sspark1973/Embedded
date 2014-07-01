f9e01000dd2b83beac719e8c9edbac26fc356c96
sspark1973 <sspark1973@gmail.com>
2014-06-30 17:00:12 -0700

    Update reg.c

--- reg.c
+++ reg.c
@@ -3,8 +3,8 @@
 #define DEBUG_PORT ((unsigned char*)(0x7E00F804))
 
 //typedef unsigned long dword;
-volatile unsigned char *pwr_cfg;
-pwr_cfg = (volatile unsigned char *)DEBUG_PORT;
+unsigned char *pwr_cfg;
+pwr_cfg = (unsigned char *)DEBUG_PORT;
 
 void set_pwr_cfg(unsigned char value)
 {
