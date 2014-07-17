e4c788139274187d7e2ef1b9023e1da872d4cf33
sspark1973 <sspark1973@gmail.com>
2014-07-17 15:29:35 -0700

    Create endian.c
    little Endian / Big Endian

---
+++ endian.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+bool8 isLittleEndian()
+{
+  int test_num = 1;
+  char *ptr;
+  
+  ptr = (char*)&test_num;
+  printf("ptr is %d\n", *ptr);
+  
+  return *ptr;
+}
+
+
+main()
+{
+  unsigned int n = 1;
+  char *p;
+  
+  p = (char *)&n;
+  
+  if(*p == 1)
+    printf("Little Endian\n");
+  else if(*(p + sizeof(int) - 1) == 1)
+    printf("Big Endian\n");
+  else
+    printf("????\n");
+    
+  isLittleEndian();
+}
